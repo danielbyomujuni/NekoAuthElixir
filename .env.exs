ca3be54616771555553dc651bb0823566dc19ea2
danielb <danielbyomujuni@nekosyndicate.com>
2025-04-22 12:26:09 -0400

    minor tweaks

--- .env.exs
+++ .env.exs
@@ -3,5 +3,5 @@
 System.put_env("SALT_ROUNDS","10")
 System.put_env("POSTGRES_USER","danielbyomujuni")
 #System.put_env(POSTGRES_PWD, "nil")
-System.put_env("POSTGRES_HOST","losthost")
+System.put_env("POSTGRES_HOST","localhost")
 System.put_env("POSTGRES_DATABASE","elixir_auth")
