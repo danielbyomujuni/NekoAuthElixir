6ac08a67af500f1fab68e8fe3cec03427077cadc
danielb <danielbyomujuni@nekosyndicate.com>
2025-04-22 11:13:51 -0400

    removed unneed files

--- example.env.exs
+++
@@ -1,7 +0,0 @@
-# .env.exs
-
-System.put_env("SALT_ROUNDS",10)
-System.put_env("POSTGRES_USER","<database user>")
-System.put_env("POSTGRES_PWD", "<database password>")
-System.put_env("POSTGRES_HOST","<database host>")
-System.put_env("POSTGRES_DATABASE","<database name>")
--- testing.env.exs
+++
@@ -1,7 +0,0 @@
-# .env.exs
-
-System.put_env("SALT_ROUNDS","10")
-System.put_env("POSTGRES_USER","danielbyomujuni")
-#System.put_env(POSTGRES_PWD, "nil")
-System.put_env("POSTGRES_HOST","localhost")
-System.put_env("POSTGRES_DATABASE","elixir_auth_test")
